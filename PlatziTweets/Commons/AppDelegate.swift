9ed5ebe8d833ec26da053e6ee6b0c5a6325c493d
Froy <froylan.almeida@gmail.com>
2021-06-30 10:25:09 -0500

    Pantallas

---
+++ PlatziTweets/Commons/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  PlatziTweets
+//
+//  Created by Field Employee on 6/29/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+  
+}
+
