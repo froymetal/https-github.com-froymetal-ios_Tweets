ae38df6b96f108aabee4da29c3bde4b1615c2c64
Froy <froylan.almeida@gmail.com>
2021-07-02 08:36:01 -0500

    Antes de validar datos

--- PlatziTweets/Modules/Welcome/LoginViewController.swift
+++ PlatziTweets/Modules/Welcome/LoginViewController.swift
@@ -6,18 +6,51 @@
 //
 
 import UIKit
+import NotificationBannerSwift
 
 class LoginViewController: UIViewController {
     //MARK: Outlets
     @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    //MARK: Actions
+    @IBAction func loginButtonAction(){
+        //funcion para quitar el teclado y quitar los focos de texto
+        view.endEditing(true)
+        //Llamada a la funcion Login
+        performLogin()
+    }
+    
+    //MARK: LOAD
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupUI()
     }
-    
+    //MARK: private functions
+    //funcion para aspecto de elementos
     private func setupUI() {
         //Redondear boton
         loginButton.layer.cornerRadius = 20
+    }
+    
+    //función para dar un error si se ha ingresado mal el mail o pass
+    private func performLogin(){
+        //validar email
+        guard let email = emailTextField.text, !email.isEmpty else{
+            NotificationBanner(title: "Error", subtitle: "Correo Inválido", style: .warning).show()
+            return
+        }
+        //validar password
+        guard let password = passwordTextField.text, !password.isEmpty else{
+            NotificationBanner(title: "Error", subtitle: "Ingrese un Password", style: .warning).show()
+            return
+        }
+        //Para seguir el segue
+        performSegue(withIdentifier: "showHome", sender: nil)
+        // Iniciar sesion aqui
+        
+        
     }
 }
--- PlatziTweets/Modules/Welcome/RegisterViewController.swift
+++ PlatziTweets/Modules/Welcome/RegisterViewController.swift
@@ -6,10 +6,23 @@
 //
 
 import UIKit
+import NotificationBannerSwift
 
 class RegisterViewController: UIViewController {
     //MARK: Outlets
     @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var namesTextField: UITextField!
+
+    //MARK: Actions
+    @IBAction func registerButtonAction(){
+        //funcion para quitar el teclado y quitar los focos de texto
+        view.endEditing(true)
+        //lamada a la funcion Register
+        performRegister()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,4 +33,30 @@ class RegisterViewController: UIViewController {
         //Redondear boton
         registerButton.layer.cornerRadius = 20
     }
+    
+    private func performRegister(){
+        //validar email
+        guard let email = emailTextField.text, !email.isEmpty else{
+            NotificationBanner(title: "Error", subtitle: "Correo Inválido", style: .warning).show()
+            return
+        }
+        //validar password
+        guard let password = passwordTextField.text, !password.isEmpty else{
+            NotificationBanner(title: "Error", subtitle: "Ingrese un Password", style: .warning).show()
+            return
+        }
+        
+        //validar nombres
+        guard let names = namesTextField.text, !names.isEmpty else{
+            NotificationBanner(title: "Error", subtitle: "Ingrese su nombre completo", style: .warning).show()
+            return
+        }
+        
+        //Para seguir el segue
+        performSegue(withIdentifier: "showHome", sender: nil)
+        
+        //Iniciar Registro
+        
+    }
+    
 }
